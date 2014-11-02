b74839dee39cb174adbff9657d683ced24701cfc
hasnain <hasnain_vohra@brown.edu>
2014-11-02 02:16:48 -0500

    working on tfidf

--- test.py
+++ test.py
@@ -3,6 +3,76 @@ import json
 import gensim
 import nltk
 from gensim import corpora, models, similarities
+import pymongo
+import pdb
+from nltk.stem.wordnet import WordNetLemmatizer
+import unicodedata
+import re
+	
+_corpus = CorpusMongo()
+_tfidf = models.TfidfModel(_corpus.vector)
+_index = similarities.SparseMatrixSimilarity(tfidf[_corpus.vector], num_features=self.ndic)
+
+class Dictionary:
+	def __init__(self):
+		f = open('/usr/share/dict/words', 'r')
+		self.words = {}
+		i = 0
+		for line in f:
+			self.words[line.strip('\n')] = i
+			i = i + 1
+		self.count = i
+		f.close()
+
+class CorpusMongo:
+	def convertToVec(self, string):
+
+	def __init__(self):
+		print 'Creating corpus of documents'
+		Dic = Dictionary()
+		lmtzr = WordNetLemmatizer()
+		
+		self.con = pymongo.Connection(host='127.0.0.1', port=3001)
+		self.db = self.con['meteor']
+		self.ideas = self.db.ideas
+		self.vector = []
+
+		for idea in self.ideas.find():
+			#Get line
+			line = idea['text']
+			line = str(unicodedata.normalize('NFKD', line).encode('ascii','ignore'))
+			#Strip of special characters
+			line = re.sub(r'[^a-z^A-Z^0-9^,^.]|\^', ' ', line)
+			line = line.lower()
+			
+			wordcount = {}
+			
+			for word in line.split(' '):
+				word = lmtzr.lemmatize(word)
+				
+				if isinstance(word, unicode):
+					word = str(unicodedata.normalize('NFKD', word).encode('ascii','ignore'))
+
+				if word in Dic.words.keys():
+					num = Dic.words[word]
+				else:
+					Dic.words[word] = Dic.count
+					num = Dic.count
+					Dic.count += 1
+				
+				if num not in wordcount.keys():
+					wordcount[num] = 1
+				else:
+					wordcount[num] = wordcount[num] + 1
+			
+			vec = []
+			for key in wordcount.keys():
+				tp =  (key, wordcount[key] + 0.0)
+				vec.append(tp)
+			
+			self.vector.append(vec)
+			self.ndic = Dic.count-1
+		#print self.corpus
 
 class MyTCPServer(SocketServer.ThreadingTCPServer):
     allow_reuse_address = True
@@ -14,6 +84,10 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
             # process the data, i.e. print it:
             #data = (self.request.recv(1024).decode('UTF-8')).strip()
             json_data = json.loads(data);
+            content = json_data['content']
+            vec = _corpus.convertToVec(content)
+            sims = index[tfidf[vec]]
+            
             #print(json_data['content']);
             #print (data);
             #tokens = nltk.word_tokenize(json_data["content"])
@@ -23,7 +97,13 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
         except Exception as e:
             print("Exception wile receiving message: ", e)
 
+
+'''
 #Implement client here
+#_tfidf = models.TfidfModel(corpus);
+print _con.database_names()
+sims = index[_tfidf[]]
+'''
 
 if __name__ == '__main__':
 	corpus = [[(0, 1.0), (1, 1.0), (2, 1.0)],
@@ -36,6 +116,9 @@ if __name__ == '__main__':
 	[(9, 1.0), (10, 1.0), (11, 1.0)],
 	[(8, 1.0), (10, 1.0), (11, 1.0)]]
 
+	#Read all things from the database and put them into corpus
+
+
 	tfidf = models.TfidfModel(corpus)
 	vec = [(0, 1), (4, 1)]
 	print(tfidf[vec])
@@ -46,5 +129,11 @@ if __name__ == '__main__':
 	print sims
 	print(list(enumerate(sims)))
 	
+	
+	
+
+	
+
+
 	server = MyTCPServer(('127.0.0.1', 6969), MyTCPServerHandler)
 	server.serve_forever()
